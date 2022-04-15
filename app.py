45597b871ad2ada68733d71ac5c671eda541518f
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-04-14 22:10:57 -0400

    up to step 3

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+debug = DebugToolbarExtension(app)
+
 responses = []
+
+@app.route("/")
+def show_survey_home():
+    return render_template("survey.html", survey=survey)
+
+@app.route("/questions/<int:q>")
+def show_question(q):
+    question = survey.questions[q]
+    return render_template("question.html", question_num = q, question=question)
